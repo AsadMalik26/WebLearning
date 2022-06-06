57c26fe3e598dc5c4070cb8a19884fa94397f7cc
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-06-06 22:09:30 +0500

    error mongoose
    Error while connection to mongo db

--- Node/MongoDB/expressdemo/express.js
+++ Node/MongoDB/expressdemo/express.js
@@ -1,50 +1,62 @@
 const express = require("express");
 const app = express();
-const food = ["kabab", "nihari", "daal rice"]
+const food = ["kabab", "nihari", "daal rice"];
 
 //to handle json type api calls
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/TestDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected with mongodb");
+  })
+  .catch(() => {
+    console.log("Error while connecting with database");
+  });
+
 
 //read or fetch requests
-app.get("/", (req,res)=>{
-    //now we send response
-    res.send("Hello this is express");
-    // now we no need to write and end for response
+app.get("/", (req, res) => {
+  //now we send response
+  res.send("Hello this is express");
+  // now we no need to write and end for response
 });
 
-app.get("/api/food", (req,res) => {
-    res.send(food);
+app.get("/api/food", (req, res) => {
+  res.send(food);
 });
 //fetch single product
-app.get("/api/food/:id", (req,res) => {
-    //if product not found then answer it
-    if(!food[req.params.id]) res.status(400).send("food item not found")
-    //status(400) is bad request code
+app.get("/api/food/:id", (req, res) => {
+  //if product not found then answer it
+  if (!food[req.params.id]) res.status(400).send("food item not found");
+  //status(400) is bad request code
 
-    // if product found
-    res.send(food[req.params.id]);
+  // if product found
+  res.send(food[req.params.id]);
 });
 
 //Put - update request
-app.put("/api/food/:id", (req,res) => {
-    //json handling required for json body
-    console.log(req.body);
-    res.send(food[req.params.id] = req.body.name);
+app.put("/api/food/:id", (req, res) => {
+  //json handling required for json body
+  console.log(req.body);
+  res.send((food[req.params.id] = req.body.name));
 });
 //delete request
-app.delete("/api/food/:id", (req,res) => {
-    //1st is starting index, 2nd is no. of records to be deleted. here the only one itself
-  food.splice(req.params.id,1);
-    res.send(food);
+app.delete("/api/food/:id", (req, res) => {
+  //1st is starting index, 2nd is no. of records to be deleted. here the only one itself
+  food.splice(req.params.id, 1);
+  res.send(food);
 });
 //Post - create request
-app.post("/api/food", (req,res) => {
-    //1st is starting index, 2nd is no. of records to be deleted. here the only one itself
+app.post("/api/food", (req, res) => {
+  //1st is starting index, 2nd is no. of records to be deleted. here the only one itself
   food.push(req.body.name);
-    res.send(food);
+  res.send(food);
 });
-
-
 
 app.listen(3030);
