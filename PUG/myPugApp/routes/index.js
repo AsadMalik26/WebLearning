525f9dec6d61fe5fb6e65b10ce66eca3fdf6ed77
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-06-17 17:31:55 +0500

    Inherit pages - Done
    we learned her how to make pages and inherit from master page by extending t to other pages
    
    Now - add stylesheet to the page

--- PUG/myPugApp/routes/index.js
+++ PUG/myPugApp/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  // res.send("Hello Asad");
+  // res.send("<h1> Hello Asad </h1>");
+  res.render("index.pug", { title: "Home Page" });
+});
+router.get("/Products", function (req, res, next) {
+  res.render("products");
+  // res.send("<h1> Hello Products </h1>");
+});
+router.get("/contactus", function (req, res, next) {
+  res.render("contactus");
 });
 
 module.exports = router;
