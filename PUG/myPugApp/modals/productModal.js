2d2845b5958a23481dd649517ccbc1dd44c9e4a3
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-06-19 01:45:30 +0500

    value condition in schema
    Validate the condition in schema to check if the price is -ve then assign 0 to price

--- PUG/myPugApp/modals/productModal.js
+++ PUG/myPugApp/modals/productModal.js
@@ -4,8 +4,11 @@ var productSchema = mongoose.Schema({
   name: String,
   price: {
     type: Number,
-    min: 0,
-    default: 0,
+    validate: function () {
+      if (this.price < 0) {
+        this.price = 0;
+      }
+    },
   },
 });
 
