455a9e787ce74a1aab8d1aca0fe3edc04350225c
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-07-03 00:07:42 +0500

    Trying to merge PUG and Express app
    But failed
    One app is working fine i.e express app
    2nd is working fine locally but not working well on server
    So, deploy 2nd app i.e pug app separately

--- PUG/myPugApp/routes/index.js
+++ PUG/myPugApp/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/h", function (req, res, next) {
   // res.render('index', { title: 'Express' });
   // res.send("Hello Asad");
   // res.send("<h1> Hello Asad </h1>");
